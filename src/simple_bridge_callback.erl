a8d9ae5532d58627cb8e8bd70c040d7e18ba6139
Jesse Gumm <sigmastar@gmail.com>
2013-10-21 17:32:01 -0500

    New config, add callback behaviour. vsn=>2.0-pre

---
+++ src/simple_bridge_callback.erl
@@ -0,0 +1,24 @@
+% vim: ts=4 sw=4 et
+-module(simple_bridge_callback).
+
+-type data()                ::  iolist().
+-type ws_data()             ::  {text, binary()} | {binary, binary()}.
+-type reply()               ::  ws_data() | [ws_data()].
+-type reason()              ::  any().
+-type full_reply()          ::  noreply
+                                | {error, reason()}
+                                | {reply, reply()},
+                                | {close, reason()},
+                                | {close, reason(), reply()}.
+
+
+-callback run(bridge())         -> {ok, Data :: data()}.
+
+-callback ws_init(bridge())     -> ok 
+                                 | {error, Reason :: any()}.
+
+-callback ws_message(ws_data()) -> full_reply().
+
+-callback ws_info(ws_data())    -> full_reply().
+
+-callback ws_terminate(reason())-> ok.
