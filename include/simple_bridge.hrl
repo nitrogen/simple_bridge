d60d063131720b9c93f7de8450997bb03f213b75
Jesse Gumm <sigmastar@gmail.com>
2013-10-30 00:14:29 -0500

    Fix typo from merge conflict

--- include/simple_bridge.hrl
+++ include/simple_bridge.hrl
@@ -8,7 +8,6 @@
 
 -record(cookie, { name, value, path="/", minutes_to_live=20 }).
 -record(header, { name, value }).
-<<<<<<< HEAD
 -record(response, { status_code=200, headers=[], cookies=[], data=[] }).
 
 %% sbw = (S)imple (B)ridge (W)rapper
@@ -23,7 +22,6 @@
 -type ipv4()		:: {integer(), integer(), integer(), integer()}.
 -type ipv8()		:: {integer(), integer(), integer(), integer(), integer(), integer(), integer(), integer()}.
 
--record(response, { statuscode=200, headers=[], cookies=[], data=[] }).
 -record(sb_uploaded_file, { original_name, temp_file, size, field_name, data }).
 -record(request_cache, {request, docroot="", body=""}).
 
