1c419ef2a9aa3c32f42ffb19e9d7c6e4333aefc0
Jesse Gumm <sigmastar@gmail.com>
2013-11-06 11:04:40 -0600

    Remove try-catch block from inets anchor

--- src/inets_bridge_modules/inets_simple_bridge_anchor.erl
+++ src/inets_bridge_modules/inets_simple_bridge_anchor.erl
@@ -2,26 +2,21 @@
 -export ([do/1]).
 	
 do(Req) ->
-	try
-		Bridge = simple_bridge:make(inets, Req),
-		ReqPath = Bridge:path(),
-		case simple_bridge_util:is_static_path(inets, ReqPath) of
-			true ->
-				Bridge2 = Bridge:set_response_file(ReqPath),
-				Bridge2:build_response();
-			false ->
-				Callout = simple_bridge_util:get_env(callout),
-				case simple_bridge_websocket:attempt_hijacking(Bridge, Callout) of
-					{hijacked, closed} ->
-						gen_tcp:close(Bridge:socket()),
-						exit(normal);
-					{hijacked, Bridge2} ->
-						Bridge2:build_response();
-					spared ->
-						Callout:run(Bridge)
-				end
-		end
-	catch
-		exit:normal -> exit(normal);
-		T:E -> error_logger:error_msg("~p:~p~nStacktrace: ~p~n",[T,E,erlang:get_stacktrace()])
+	Bridge = simple_bridge:make(inets, Req),
+	ReqPath = Bridge:path(),
+	case simple_bridge_util:is_static_path(inets, ReqPath) of
+		true ->
+			Bridge2 = Bridge:set_response_file(ReqPath),
+			Bridge2:build_response();
+		false ->
+			Callout = simple_bridge_util:get_env(callout),
+			case simple_bridge_websocket:attempt_hijacking(Bridge, Callout) of
+				{hijacked, closed} ->
+					gen_tcp:close(Bridge:socket()),
+					exit(normal);
+				{hijacked, Bridge2} ->
+					Bridge2:build_response();
+				spared ->
+					Callout:run(Bridge)
+			end
 	end.
