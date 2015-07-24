1a0c358140b6c5e6304152f37cf91e25c67b9b86
Jesse Gumm <gumm@sigma-star.com>
2015-07-23 22:12:41 -0500

    Add test for deep static files

--- test/simple_bridge_SUITE.erl
+++ test/simple_bridge_SUITE.erl
@@ -16,15 +16,18 @@
 	request_method_post/1,
 	request_body/1,
 	query_params/1,
-	post_params/1
+	post_params/1,
+	static/1,
+	deep_static/1,
+	deeper_static/1
 ]).
 
 all() -> [{group, main}].
 
 groups() ->
 	[{main, 
-		[parallel, {repeat, 10}],
-		[peer_ip, request_method_get, request_method_post, request_body, protocol, path, query_params, post_params]
+		[parallel, {repeat, 1}],
+		[peer_ip, request_method_get, request_method_post, request_body, protocol, path, query_params, post_params, static, deep_static, deeper_static]
 	}].
 
 init_per_group(main, Config) ->
@@ -64,6 +67,21 @@ query_params(_) ->
 
 post_params(_) ->
 	"[{<<\"a\">>,<<\"1\">>},{<<\"b\">>,<<\"2\">>}]" = post("post_params", "a=1&b=2").
+	
+static(_) ->
+	"1234\n" = get_static("static/test_static.txt").
+
+deep_static(_) ->
+	"1\n" = get_static("static/deep/depth1.txt").
+
+deeper_static(_) ->
+	"2\n" = get_static("static/deep/deeper/depth2.txt").
+
+get_static(File) ->
+	URL = "http://127.0.0.1:8000/" ++ File,
+	{ok, {_, _, Val}} = httpc:request(URL),
+	Val.
+
 
 request(Path) ->
 	URL = "http://127.0.0.1:8000/" ++ Path,
