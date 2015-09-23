9ad3c380531cdbc99b8978ef783def89f731b9da
danikp <dmitry.polyanovsky@gmail.com>
2015-09-23 10:51:56 +0300

    cowboy ignoring IP 'address' setting has ben actually ignored for cowboy and server started on all IPs available in any case. Also, ranch accept IP only as tuple, so convert them to such before passing to ranch.

--- src/cowboy_bridge_modules/cowboy_simple_bridge_sup.erl
+++ src/cowboy_bridge_modules/cowboy_simple_bridge_sup.erl
@@ -25,9 +25,10 @@ init([]) ->
     application:start(cowlib),
     application:start(cowboy),
     {Address, Port} = simple_bridge_util:get_address_and_port(cowboy),
+    IP = simple_bridge_util:parse_ip(Address),
 
-    io:format("Starting Cowboy Server on ~s:~p~n",
-              [Address, Port]),
+    io:format("Starting Cowboy Server on ~p:~p~n",
+              [IP, Port]),
 
     Dispatch = generate_dispatch(),
     io:format("Using Cowboy Dispatch Table:~n  ~p~n",[Dispatch]),
@@ -37,7 +38,7 @@ init([]) ->
         {max_keepalive, 100}
     ],
 
-    Args = [http, 100, [{port, Port}], Opts],
+    Args = [http, 100, [{ip, IP}, {port, Port}], Opts],
     Restart = permanent,
     Shutdown = 5000,
     Type = worker,
