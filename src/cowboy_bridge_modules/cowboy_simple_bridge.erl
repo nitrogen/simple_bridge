e5e9d3b33dcb71b2d4c5ef8e8e963ecfa162167f
João Henrique Ferreira de Freitas <joao.freitas@ekinops.com>
2022-11-11 13:05:10 -0300

    Fix dialyzer errors for cowboy 2.9.0

--- src/cowboy_bridge_modules/cowboy_simple_bridge.erl
+++ src/cowboy_bridge_modules/cowboy_simple_bridge.erl
@@ -162,9 +162,7 @@ recv_from_socket(_Length, _Timeout, ReqKey) ->
             Data;
         {more, Data, NewReq} ->
             put_key(ReqKey, RequestCache#request_cache{request = NewReq}),
-            Data;
-        {error, Reason} ->
-            exit({error, Reason}) %% exit(normal) instead?
+            Data
     end.
 
 
@@ -174,7 +172,7 @@ protocol_version(ReqKey) ->
     case Version of
         'HTTP/1.1' -> {1, 1};
         'HTTP/1.0' -> {1, 0};
-        {H, L} -> {H, L}
+        'HTTP/2' -> {2, 0}
     end.
 
 %% RESPONSE
